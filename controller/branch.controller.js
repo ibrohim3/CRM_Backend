911577a2b28aad939ecfdef2c092b1951cf44e20
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-11 22:32:10 +0500

    done

--- controller/branch.controller.js
+++ controller/branch.controller.js
@@ -8,10 +8,11 @@ const createBranch = async (req, res) => {
             address,
             call_number
         } = req.body
-        if (!name || !address || !call_number) {
+        const existingBranch = await Branch.findOne({ name })
+        if (existingBranch) {
             return res.status(400).json({
                 success: false,
-                message: "kerakli maydonlar to'ldirilmadi."
+                message: "Bu nom bilan branch mavjud."
             })
         } else {
             const newBrach = await Branch.create({
@@ -34,6 +35,58 @@ const createBranch = async (req, res) => {
     }
 }
 
+// Get All
+const getBranches = async (req, res) => {
+    try {
+        const branches = await Branch.find({})
+        if (!branches || branches.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Branch topilamdi"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Branches topildi",
+            data: branches
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Srver xatosi",
+            error: error.message
+        })
+    }
+
+}
+
+// Get one
+const getBranch = async (req, res) => {
+    try {
+        const { id } = req.params
+        const branch = await Branch.findById(id)
+
+        if (!branch) {
+            return res.status(404).json({
+                success: false,
+                message: "Branch topilmadi."
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Branch topildi",
+            data: branch
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server xatosi",
+            error: error.message
+        })
+    }
+}
 module.exports = {
-    createBranch
+    createBranch,
+    getBranches,
+    getBranch
 }
--- routes/branchRoute.js
+++ routes/branchRoute.js
@@ -2,9 +2,15 @@ const { Router } = require("express")
 const branch = Router()
 
 const {
-    createBranch
+    createBranch,
+    getBranches,
+    getBranch
 } = require("../controller/branch.controller")
 
 branch.post("/register", createBranch)
+
+branch.get("/", getBranches)
+
+branch.get("/getById/:id", getBranch)
 
 module.exports = { branch }
