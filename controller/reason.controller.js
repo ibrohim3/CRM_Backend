47c2fa785e7cf8a2d992abb2f1e460a304d1cf39
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-13 20:09:00 +0500

    done

--- controller/reason.controller.js
+++ controller/reason.controller.js
@@ -1,4 +1,4 @@
-const Reason = require("../model/reasonSchema")
+const { Reason } = require("../model/reasonSchema")
 
 // POST
 const createReasonLid = async (req, res) => {
@@ -28,7 +28,117 @@ const createReasonLid = async (req, res) => {
     }
 }
 
+// Get all
+const getAllReasons = async (req, res) => {
+    try {
+        const reasons = await Reason.find({})
+        if (!reasons) {
+            return res.status(404).json({
+                success: false,
+                message: "Reason lid topilamdi"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Topildi",
+            count: reasons.length,
+            data: reasons
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server xatosi",
+            error: error.message
+        })
+    }
+}
 
+// Get one 
+const getOneReason = async (req, res) => {
+    try {
+
+        const { id } = req.params
+        const reason = await Reason.findById(id)
+        if (!reason || reason.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "reason topilmadi"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Topildi",
+            data: reason
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server xatosi",
+            error: error.message
+        })
+    }
+}
+
+// Update
+const updateReason = async (req, res) => {
+    try {
+        const { id } = req.params
+        const {
+            reason_lid
+        } = req.body
+        const updateData = {
+            reason_lid
+        }
+        const updatedReason = await Reason.findByIdAndUpdate(
+            id, updateData, { new: true }
+        )
+        if (!updatedReason) {
+            return res.status(404).json({
+                success: false,
+                message: "Topilmadi",
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Yangilandi",
+            updated: updatedReason
+        })
+    } catch (error) {
+        return res.status({
+            success: false,
+            message: "server xatosi",
+            error: error.message
+        })
+    }
+}
+
+// Delete
+const deleteReason = async (req, res) => {
+    try {
+        const { id } = req.params
+        const removed = await Reason.findByIdAndDelete(id)
+        if (!removed) {
+            return res.status(404).json({
+                success: false,
+                message: "Topilmadi"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "O'chirildi",
+            removed: removed
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server xatosi"
+        })
+    }
+}
 module.exports = {
-    createReasonLid
+    createReasonLid,
+    getAllReasons,
+    getOneReason,
+    updateReason,
+    deleteReason
 }
--- routes/reasonRoute.js
+++ routes/reasonRoute.js
@@ -2,8 +2,16 @@ const { Router } = require("express")
 const reason = Router()
 
 const {
-    createReasonLid
+    createReasonLid,
+    getAllReasons,
+    getOneReason,
+    updateReason,
+    deleteReason
 } = require("../controller/reason.controller")
 
 reason.post("/register", createReasonLid)
+reason.get("/", getAllReasons)
+reason.get("/getById/:id", getOneReason)
+reason.patch("/update/:id", updateReason)
+reason.delete("/delete/:id", deleteReason)
 module.exports = { reason }
