d0690e111a7657d2cc791fd7407b9c56ed81409c
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-16 20:28:34 +0500

    done

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const payment = Router()
-
 const {
     createPayment,
     getPayments,
@@ -8,15 +7,13 @@ const {
     updatePayment,
     deletePayment
 } = require("../controller/payment.controller")
+const { validate } = require("../middlewares/validate")
+const { createPaymentValidation, updatePaymentValidation } = require("../validation/payment.validation")
+const { idParamValidationSchema } = require("../validation/common.validation.js")
 
-payment.post("/register", createPayment)
-
+payment.post("/register", validate(createPaymentValidation, "body"), createPayment)
 payment.get("/", getPayments)
-
-payment.get("/byId/:id", getPayment)
-
-payment.patch("/update/:id", updatePayment)
-
-payment.delete("/delete/:id", deletePayment)
-
+payment.get("/byId/:id", validate(idParamValidationSchema, "params"), getPayment)
+payment.patch("/update/:id", validate(updatePaymentValidation, "body"), updatePayment)
+payment.delete("/delete/:id", validate(idParamValidationSchema, "params"), deletePayment)
 module.exports = { payment }
---
+++ validation/payment.validation.js
@@ -0,0 +1,27 @@
+const Joi = require("joi")
+
+const createPaymentValidation = Joi.object({
+    student_id: Joi.number().integer().positive().required(),
+    payment_last_date: Joi.date().required(),
+    payment_date: Joi.date().greater("now").required().messages({
+        "any.required": "Lesson date is required",
+        "date.greater": "Lesson date must be in the future"
+    }),
+    price: Joi.number().positive().required(),
+    is_paid: Joi.boolean().default(true),
+    total_attent: Joi.number().integer().positive().required()
+})
+
+const updatePaymentValidation = Joi.object({
+    student_id: Joi.number().integer().positive().optional(),
+    payment_last_date: Joi.date().optional(),
+    payment_date: Joi.date().greater("now").optional().messages({
+        "any.required": "Lesson date is required",
+        "date.greater": "Lesson date must be in the future"
+    }),
+    price: Joi.number().positive().optional(),
+    is_paid: Joi.boolean().default(true),
+    total_attent: Joi.number().integer().positive().optional()
+})
+
+module.exports = { createPaymentValidation, updatePaymentValidation }
