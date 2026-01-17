aeecc5e198a137dde48d9fc955be583b2a626e47
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-17 21:43:49 +0500

    swagger added!

--- routes/branchRoute.js
+++ routes/branchRoute.js
@@ -18,16 +18,68 @@ const {
 } = require("../validation/common.validation.js")
 const { validate } = require("../middlewares/validate.js")
 
+/**
+ * @swagger
+ * tags: 
+ *   - name: Branch
+    *     description: Branch
+ */
+/**
+ * @swagger
+ * /branch:
+ *  post:
+ *    summary: Branch qoshish
+ *    tags: 
+ *      - Branch
+ *    description: Branch qoshish
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - name 
+ *            properties:
+ *              name:
+ *                type: string
+ *              address:
+ *                type: string
+ *              call_number:
+ *                type: string
+ *    responses:
+ *      201:
+ *        description: Muvaffqiyatli yaratildi
+ *      400: 
+ *        description: Bu nom bilan branch mavjud yoki validatsiya xatosi
+ *      500: 
+ *        description: Server xatosi 
+ *           
+ */
+branch.post("/", validate(createBranchValidation), createBranch)
 
-branch.post("/register", validate(createBranchValidation), createBranch)
-
+/**
+ * @swagger
+ * /branch:
+ *   get:
+ *     summary: Barcha branch malumotini olish
+ *     tags:
+ *       - Branch
+ *     responses:
+ *       200: 
+ *         description: Muvaffaqiyatli olindi
+ *       404: 
+ *         description: Branch malumotlari topilmadi
+ *       500: 
+ *         description: Server xatosi
+ */
 branch.get("/", getBranches)
 
-branch.get("/getById/:id", validate(idParamValidationSchema), getBranch)
+branch.get("/:id", validate(idParamValidationSchema), getBranch)
 
-branch.patch("/update/:id", validate(updateBranchValidation), updateBranch)
+branch.patch("/:id", validate(updateBranchValidation), updateBranch)
 
-branch.delete("/delete/:id", validate(idParamValidationSchema), deleteBranch)
+branch.delete("/:id", validate(idParamValidationSchema), deleteBranch)
 
 branch.get("/search", validate(searchValidationSchema), searchBranch)
 module.exports = { branch }
